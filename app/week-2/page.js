050e5d87f9ca3a91993568411dc3cdcae3b193da
Hamdy Ibrahim <ha_madien@yahoo.com>
2025-09-11 08:35:57 -0600

    adding Title component

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -1,7 +1,9 @@
+import Title from "./title.js";
 export default function Page() {
   return (
     <main>
       <h1>Week 2 route Page</h1>
+      <Title />
       <p>Welcome to week 2 page</p>
     </main>
   );
