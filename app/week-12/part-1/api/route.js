57abbcb7c065eb42f8cec9e6d05fe5d6676daa5c
Hamdy Ibrahim <ha_madien@yahoo.com>
2025-11-25 09:22:59 -0700

    week 12

---
+++ app/week-12/part-1/api/route.js
@@ -0,0 +1,9 @@
+// GET function maps to the GET HTTP method
+export function GET() {
+  const num = Math.floor(Math.random() * 3) + 1;
+  const url = `http://localhost:3000/images/dog${num}.jpg`;
+
+  // note: this API returns text, not JSON
+  // my design, my rules
+  return new Response(url);
+}
